1944c652407ba09047bcb1da3e6e3d9b57733e17
Greg Noethlich <gmnoethlich@gmail.com>
2023-02-06 14:42:06 -0600

    Week 1 Workshop complete with bonus tasks

--- week_1/battlegame.py
+++ week_1/battlegame.py
@@ -1,14 +1,17 @@
 wizard = "Wizard"
 elf = "Elf"
 human = "Human"
+orc = "Orc"
 
 wizard_hp = 70
 elf_hp = 100
 human_hp = 150
+orc_hp = 180
 
 wizard_damage = 150
 elf_damage = 100
 human_damage = 20
+orc_damage = 30
 
 dragon_hp = 300
 dragon_damage = 50
@@ -16,24 +19,35 @@ dragon_damage = 50
 while True:
     print("1)", wizard)
     print("2)", elf)
-    print("3)" ,human)
+    print("3)" , human)
+    print("4)", orc)
+    print("5) Exit")
     character = input("Choose your character: ")
-    if character == "1":
+    if character == "1" or character.casefold() == "wizard":
         character = wizard
         my_hp = wizard_hp
         my_damage = wizard_damage
         break
-    if character == "2":
+    elif character == "2" or character.casefold() == "elf":
         character = elf
         my_hp = elf_hp
         my_damage = elf_damage
         break
-    if character == "3":
+    elif character == "3" or character.casefold() == "human":
         character = human
         my_hp = human_hp
         my_damage = human_damage
         break
-    print("Unknown Character")
+    elif character == "4" or character.casefold() == "orc":
+        character = orc
+        my_hp = orc_hp
+        my_damage = orc_damage
+        break
+    elif character == "5" or character.casefold() == "exit":
+        exit("Exited Game")
+    else:
+        print("Unknown Character")
+        break
 print("You have chosen the character: ", character)
 print("Health ", my_hp)
 print("Damage: ", my_damage)

1daceae8cb8563830dd4daab1ac57b7cdc6ef466
Greg Noethlich <gmnoethlich@gmail.com>
2023-02-14 16:02:58 -0600

    Class instance methods

--- week_4/using_classes1.py
+++ week_4/using_classes1.py
@@ -20,4 +20,10 @@ player1 = Player("Aaron", 1200)
 player2 = Player("Irene", 1300)
 
 print("P1:", player1.name, "-- HP:", player1.hp, "-- SCORE: ", player1.score)
-print("P1:", player2.name, "-- HP:", player2.hp, "-- SCORE: ", player2.score)
+print("P2:", player2.name, "-- HP:", player2.hp, "-- SCORE: ", player2.score)
+
+player1.hp += 500
+player1.score += 10
+
+print("P1:", player1.name, "-- HP:", player1.hp, "-- SCORE: ", player1.score)
+print("P2:", player2.name, "-- HP:", player2.hp, "-- SCORE: ", player2.score)

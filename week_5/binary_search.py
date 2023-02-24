0a156519458864555583ea3d8fbd819dc0df2312
Greg Noethlich <gmnoethlich@gmail.com>
2023-02-23 21:01:13 -0600

    binary search

---
+++ week_5/binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(the_list, target):
+    lower_bound = 0
+    upper_bound = len(the_list) - 1
+
+    while lower_bound <= upper_bound:
+        pivot = (lower_bound + upper_bound) //2
+        pivot_value = the_list[pivot]
+
+        if pivot_value == target:
+            return pivot
+        if pivot_value > target:
+            upper_bound = pivot - 1
+        else:
+            lower_bound = pivot + 1

d5a3c94cb83fefcf502c599f1f497e1420d0012b
Greg Noethlich <gmnoethlich@gmail.com>
2023-02-25 21:49:54 -0600

    CC linear search on dictionary

--- week_5/binary_search.py
+++ week_5/binary_search.py
@@ -15,7 +15,7 @@ def binary_search(the_list, target):
 
     return -1
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(binary_search(my_list, 10))
-print(binary_search(my_list, 4))
-print(binary_search(my_list, 33))
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(binary_search(test_list, 10))
+print(binary_search(test_list, 4))
+print(binary_search(test_list, 33))
